3992ba5afffdd7a992336908f3d7a33a7e6bbb50
Ricky Marcon <ricky.marcon@gmail.com>
2022-08-30 00:12:11 +0100

    fix: missing font weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "@fontsource/inter"
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/800.css";
 
 import { ChakraProvider } from "@chakra-ui/react"
 import { CacheProvider } from "@emotion/react"
